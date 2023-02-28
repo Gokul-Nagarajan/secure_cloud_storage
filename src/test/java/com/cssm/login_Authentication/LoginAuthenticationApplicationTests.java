fd343f9657979245deacbca6c1778f11232e6907
Gokul Nagarajan <gokulnagarajang@gmail.com>
2023-02-28 09:08:22 +0530

    added project files

---
+++ src/test/java/com/cssm/login_Authentication/LoginAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cssm.login_Authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
